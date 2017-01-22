1d8cded13d008e56a6472089a30a7a014c1f462e
David Monschein <david.monschein@novatec-gmbh.de>
2017-01-22 18:09:25 +0100

    Added current iObserve version as child project.

---
+++ thesis.iobserve/make-local-repo.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+BINDIR=$(cd "$(dirname "$0")"; pwd)
+
+LIBS=~/.p2/pool/plugins
+
+LIST=`cat << EOF
+de.uka.ipd.sdq.identifier
+org.palladiosimulator.pcm
+de.uka.ipd.sdq.units
+de.uka.ipd.sdq.stoex
+de.uka.ipd.sdq.probfunction
+protocom.extension
+org.eclipse.emf.cdo
+org.eclipse.emf.cdo.common
+org.eclipse.ocl
+org.eclipse.ocl.common
+org.eclipse.ocl.ecore
+org.eclipse.net4j
+org.eclipse.net4j.util
+org.eclipse.equinox.common
+lpg.runtime.java
+EOF`
+
+MVNDIR="${BINDIR}/mvn-repo"
+
+if [ ! -d "${MVNDIR}" ] ; then
+	mkdir "${MVNDIR}"
+fi
+
+export GRADLE_DEPENDENCIES="${BINDIR}/gradle.dependencies"
+
+NEWLINE=$'\n'
+
+rm -f "${GRADLE_DEPENDENCIES}"
+touch "${GRADLE_DEPENDENCIES}"
+
+for I in $LIST ; do
+	echo "Op $I"
+	for P in `find ${LIBS} -name "${I}_*.jar"` ; do
+		echo "$P"
+		if [ -f "$P" ] ; then
+			N=`basename $P`
+			ARTIFACT=`echo $N | sed 's/^\(.*\)_.*$/\1/'`
+			VERSION=`echo $N | sed 's/^.*_\(.*\)\.jar$/\1/'`
+			echo $N
+		#	echo $ARTIFACT
+		#	echo $VERSION
+			mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile="$P" \
+				-DgroupId=extra -DartifactId=$ARTIFACT -Dpackaging=jar -Dversion=$VERSION \
+				-DlocalRepositoryPath=mvn-repo
+			echo "compile 'extra:${ARTIFACT}:${VERSION}'" >> "${GRADLE_DEPENDENCIES}"
+		fi
+	done
+done
+
+# end
